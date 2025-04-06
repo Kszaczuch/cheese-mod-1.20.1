448b5b8e9f29eca9aba6be23e00b26a141f0aa76
Kszaczuch <b.kotlik2009@gmail.com>
2025-04-06 14:10:48 +0200

    Initial commit

---
+++ src/main/java/net/kszaczuch/cheese_mod/CheeseModClient.java
@@ -0,0 +1,10 @@
+package net.kszaczuch.cheese_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CheeseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
